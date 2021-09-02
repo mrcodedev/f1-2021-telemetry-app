41cf56b514c7dbbd814829c2056c654ffa2d3fe6
mrcodedev <mrcodedev@gmail.com>
2021-09-02 02:03:23 +0200

    chore: added icon teams to classification

--- src/app/src/components/modules/classification/module-classification.tsx
+++ src/app/src/components/modules/classification/module-classification.tsx
@@ -29,22 +29,25 @@ export class F1ModuleClassification {
             </td>
             <td>NET</td>
             <td>Red Bull Racing Honda</td>
-            <td>logo</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/redbull.jpg" />
+            </td>
           </tr>
           <tr>
-            <td>
+            <td class="position">
               <span class="square f1-text-bold">2</span>
             </td>
             <td>
-              {" "}
               <span class="color-team" />
             </td>
             <td>
-              Fernando <span class="f1-text-bold">ALONSO</span>
+              Sergio <span class="f1-text-bold">PEREZ</span>
             </td>
-            <td>ES</td>
-            <td>Alpine Renault</td>
-            <td>logo</td>
+            <td>NET</td>
+            <td>Red Bull Racing Honda</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/redbull.jpg" />
+            </td>
           </tr>
           <tr>
             <td>
@@ -55,11 +58,13 @@ export class F1ModuleClassification {
               <span class="color-team" />
             </td>
             <td>
-              Carlos <span class="f1-text-bold">SAINZ</span>
+              Fernando <span class="f1-text-bold">ALONSO</span>
             </td>
             <td>ES</td>
-            <td>Ferrari</td>
-            <td>logo</td>
+            <td>Alpine Renault</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alpine.jpg" />
+            </td>
           </tr>
           <tr>
             <td>
@@ -70,11 +75,285 @@ export class F1ModuleClassification {
               <span class="color-team" />
             </td>
             <td>
+              Esteban <span class="f1-text-bold">OCON</span>
+            </td>
+            <td>FRA</td>
+            <td>Alpine Renault</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alpine.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">5</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Carlos <span class="f1-text-bold">SAINZ</span>
+            </td>
+            <td>ES</td>
+            <td>Ferrari</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/ferrari.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">6</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Charles <span class="f1-text-bold">LECLERC</span>
+            </td>
+            <td>FRA</td>
+            <td>Ferrari</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/ferrari.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">7</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
               Lando <span class="f1-text-bold">NORRIS</span>
             </td>
             <td>GB</td>
             <td>Mclaren Mercedes</td>
-            <td>logo</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/mclaren.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">8</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Daniel <span class="f1-text-bold">RICCIARDO</span>
+            </td>
+            <td>ITA</td>
+            <td>Mclaren Mercedes</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/mclaren.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">9</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Lewis <span class="f1-text-bold">HAMILTON</span>
+            </td>
+            <td>GB</td>
+            <td>Mercedes</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/mercedes.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">10</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Valteri <span class="f1-text-bold">BOTTAS</span>
+            </td>
+            <td>NRW</td>
+            <td>Mercedes</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/mercedes.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">11</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Kimi <span class="f1-text-bold">RAIKKONEN</span>
+            </td>
+            <td>GB</td>
+            <td>Alfa Romeo</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alfa-romeo.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">12</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Antonio <span class="f1-text-bold">GIOVINAZZI</span>
+            </td>
+            <td>NRW</td>
+            <td>Alfa Romeo</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alfa-romeo.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">13</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Mick <span class="f1-text-bold">Haas</span>
+            </td>
+            <td>GB</td>
+            <td>Haas</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/haas.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">14</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Nikita <span class="f1-text-bold">MAZEPIN</span>
+            </td>
+            <td>NRW</td>
+            <td>Haas</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/haas.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">15</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Yuki <span class="f1-text-bold">TSUNODA</span>
+            </td>
+            <td>GB</td>
+            <td>Alpha Tauri</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alpha-tauri.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">16</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Pierre <span class="f1-text-bold">GASLY</span>
+            </td>
+            <td>NRW</td>
+            <td>Alpha Tauri</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/alpha-tauri.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">17</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              George <span class="f1-text-bold">RUSELL</span>
+            </td>
+            <td>GB</td>
+            <td>Williams</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/williams.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">18</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Nicholas <span class="f1-text-bold">Latify</span>
+            </td>
+            <td>NRW</td>
+            <td>Williams</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/williams.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">19</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Sebastian <span class="f1-text-bold">VETTEL</span>
+            </td>
+            <td>GB</td>
+            <td>Aston Martin</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/aston-martin.jpg" />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="square f1-text-bold">20</span>
+            </td>
+            <td>
+              {" "}
+              <span class="color-team" />
+            </td>
+            <td>
+              Lance <span class="f1-text-bold">STROLL</span>
+            </td>
+            <td>NRW</td>
+            <td>Aston Martin</td>
+            <td class="logo-team">
+              <img src="/assets/img/teams/final/aston-martin.jpg" />
+            </td>
           </tr>
         </tbody>
       </table>
